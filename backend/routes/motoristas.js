2a2022c24da5e4acf73ea283f6612a2f0aac49d2
lorena.macedo@aluno.ifsp.edu.br <lorena.macedo@aluno.ifsp.edu.br>
2023-10-10 17:12:58 -0300

    iniciando as rotas dos motoristas

--- backend/routes/motoristas.js
+++ backend/routes/motoristas.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const { PrismaClient } = require("@prisma/client");
+
+const prisma = new PrismaClient();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const motoristas = await prisma.motorista.findMany();
+  res.json(motoristas);
+});
+
+/* POST home page. */
+router.post("/cadastrar", async (req, res, next) =>{
+  try {
+    const { nome, nascimento, sexo, foto, admissao } = req.body;
+
+    const novoMotorista = await prisma.motorista.create({
+      data: {
+        nome,
+        nascimento, 
+        sexo, 
+        foto, 
+        admissao 
+      },
+    });
+
+    res.json(novoMotorista);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Erro ao cadastrar motorista."});
+  }
 });
 
 module.exports = router;

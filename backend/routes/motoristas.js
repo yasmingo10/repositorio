2d5b97325d71ccc5769633f2011d7462d6049d00
lorislolo <lorena.macedo@aluno.ifsp.edu.br>
2023-10-14 21:00:44 -0300

    crud do motorista feito, sem correção de error ainda

--- backend/routes/motoristas.js
+++ backend/routes/motoristas.js
@@ -3,13 +3,18 @@ const router = express.Router();
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-/* GET home page. */
+/* GET*/
 router.get('/', async function(req, res, next) {
-  const motoristas = await prisma.motorista.findMany();
-  res.json(motoristas);
+  try {
+    const motoristas = await prisma.motorista.findMany();
+    res.json(motoristas);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({error: "Erro ao recuperar motoristas."})
+  }
 });
 
-/* POST home page. */
+/* POST */
 router.post("/cadastrar", async (req, res, next) => {
   try {
     const { nome, nascimento, sexo, foto, admissao } = req.body;
@@ -27,8 +32,64 @@ router.post("/cadastrar", async (req, res, next) => {
     res.json(novoMotorista);
   } catch (error) {
     console.error(error);
-    res.status(500).json({ error: "Erro ao cadastrar motorista."});
+    res.status(500).json({ error: "Erro ao cadastrar motorista!"});
   }
+});
+
+/*GET através do ID*/
+router.get('/:id', async (req, res) =>{
+  try {
+    const id = Number(req.params.id);
+    const idMotorista = await prisma.motorista.findUnique({
+      where: {
+        id: id
+      }
+    });
+    res.json(idMotorista)
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({ error: "Motorista não encontrado!"})
+  }
+});
+
+/*Metodo patch*/
+router.patch('/:id', async (req, res) =>{
+  try {
+  const id = Number(req.params.id);
+  const data = req.body;
+  const atualizarMotorista = await prisma.motorista.update({
+    where: {
+      id: id
+    },
+    data: data
+  });
+  res.json(atualizarMotorista);
+}
+catch (error){
+  console.log(error);
+  res.status(404).json({ error: "Erro ao atualizar motorista!"});
+}
+});
+
+/*Delete*/
+router.delete('/:id', async (req, res)=>{
+  try {
+    const id = Number(req.params.id);
+    await prisma.motorista.delete({
+      where:{
+        id: id
+      }
+    });
+    res.status(204).end();
+  } catch (error) {
+    console.log(error);
+  res.status(500).json({error: "Erro ao excluir motorista."})
+  }
+});
+
+/*Para todas as rotas não existentes*/
+router.all('*', (req, res) =>{
+  res.status(501).end();
 });
 
 module.exports = router;

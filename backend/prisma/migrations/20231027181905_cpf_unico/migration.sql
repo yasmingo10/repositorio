756e0c9b8c6c9f2b01f964554142b5262f205a3f
lorislolo <lorena.macedo@aluno.ifsp.edu.br>
2023-10-30 17:40:41 -0300

    ajustes no admin

--- backend/prisma/migrations/20231027181905_cpf_unico/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[cpf]` on the table `passageiro` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `passageiro_cpf_key` ON `passageiro`(`cpf`);

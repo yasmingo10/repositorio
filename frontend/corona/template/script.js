9277612dd93ce719f7c3b10e5feb4576eda65ea1
lorislolo <lorena.macedo@aluno.ifsp.edu.br>
2023-11-12 14:35:09 -0300

    tentativa de conectar os servidores

---
+++ frontend/corona/template/script.js
@@ -0,0 +1,26 @@
+document.getElementById("formMotoristas").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const jsonData = {};
+
+    formData.forEach((value, key) => {
+        jsonData[key] = value;
+    });
+
+    fetch("http://localhost:5000/motoristas/cadastrar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(jsonData),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Dados enviados com sucesso:", data);
+            // Faça algo com a resposta do backend, se necessário
+        })
+        .catch(error => {
+            console.error("Erro ao enviar dados:", error);
+        });
+});

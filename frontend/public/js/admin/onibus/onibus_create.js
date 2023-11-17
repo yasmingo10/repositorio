fa95ff243411273ea97da77aa5d637f24bcd0147
lorislolo <lorena.macedo@aluno.ifsp.edu.br>
2023-11-17 15:25:10 -0300

    conexão

--- frontend/public/js/admin/onibus/onibus_create.js
+++ frontend/public/js/admin/onibus/onibus_create.js
@@ -1,21 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const form = document.querySelector("#formMotoristas");
+    const form = document.querySelector("#formOnibus");
     form.addEventListener("submit", async (event) => {
         event.preventDefault();
         const formData = new FormData(form);
         if (form.checkValidity()) {
             try {
                 const response = await axios.post(
-                    "http://localhost:5000/motoristas/cadastrar",
-                    formData,
-                    {
-                        headers: {
-                          'Content-Type': 'multipart/form-data'
-                        }
-                      });
+                    "http://localhost:5000/onibus/cadastrar",
+                    formData,);
                 console.log(response.data);
                 form.reset();
-                alert(`Motorista cadastrado com sucesso! id=${response.data.motorista.id}`)
+                alert(`Onibus cadastrado com sucesso! id=${response.data.motorista.id}`)
                 // window.location.href = `http://localhost:4000/admin/motorista/:id`;
             } catch (error) {
                 console.log(error.message);
--- frontend/routes/admin.js
+++ frontend/routes/admin.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
+
+router.get('/', function(req, res, next) {
+    res.sendFile("index.html", { root: 'views/admin' });
+});
+
 router.get('/motorista/cadastrar', function(req, res, next) {
     res.sendFile("cadastrar.html", { root: 'views/admin/motorista' });
+});
+
+router.get('/onibus/cadastrar', function(req, res, next) {
+    res.sendFile("cadastrar.html", { root: 'views/admin/onibus' });
 });
 
 module.exports = router;
